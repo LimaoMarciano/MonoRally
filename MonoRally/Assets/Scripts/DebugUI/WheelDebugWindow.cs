505ac269da004e09c09ebd180caee56e2493273a
Marcelo <marceloapsilva@gmail.com>
2016-11-10 23:15:30 -0200

    - Adicionei janela de debug para pneu e freio

---
+++ MonoRally/Assets/Scripts/DebugUI/WheelDebugWindow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelDebugWindow : MonoBehaviour {
+
+	public Robot robot;
+	public Fillbar angularVelocity;
+	public DebugValue velocity;
+	public DebugBoolean groundedBool;
+
+	// Use this for initialization
+	void Start () {
+		angularVelocity.SetLabel ("AngVelocity");
+		velocity.SetLabel ("Velocity");
+		groundedBool.SetLabel ("Is grounded?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		angularVelocity.SetMaxValue (robot.engine.maxSpeed / robot.transmission.GetCurrentGearRatio ());
+		angularVelocity.value = Mathf.Abs(robot.wheel.GetAngularVelocity ());
+		velocity.SetValue (robot.wheel.GetCurrentSpeed (), false);
+		groundedBool.SetValue (robot.wheel.isGrounded);
+	}
+}
