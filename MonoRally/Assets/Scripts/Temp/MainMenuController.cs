0293727cdd3083489f8c3070f4a28c56b7882390
Marcelo <marceloapsilva@gmail.com>
2016-11-16 23:09:30 -0200

    - Adicionei menu principal

---
+++ MonoRally/Assets/Scripts/Temp/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void Quit () {
+		Application.Quit ();
+	}
+
+	public void LoadScene (string scene) {
+		SceneManager.LoadScene (scene);
+	}
+
+}
